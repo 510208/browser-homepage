2d19872c12e2ce45c37a35a9d30849f327341fdb
510208 Sam <xux510208@gmail.com>
2025-05-09 12:35:45 +0000

    feat: 更新Gitpod設定，並更改首頁預設的連結

--- components/card/shortcuts/types.ts
+++ components/card/shortcuts/types.ts
@@ -7,12 +7,14 @@ export type Shortcut = {
 
 // 預設捷徑
 export const defaultShortcuts: Shortcut[] = [
-  { id: "1", name: "Google", url: "https://google.com" },
-  { id: "2", name: "Facebook", url: "https://facebook.com" },
-  { id: "3", name: "Twitter", url: "https://twitter.com" },
-  { id: "4", name: "Instagram", url: "https://instagram.com" },
-  { id: "5", name: "LinkedIn", url: "https://linkedin.com" },
-  { id: "6", name: "GitHub", url: "https://github.com" },
+  { id: "1", name: "SamHacker Blog", url: "https://samhacker.xyz" },
+  { id: "2", name: "GitHub", url: "https://github.com/510208" },
+  { id: "3", name: "Discord", url: "https://discord.com" },
+  { id: "4", name: "YT Music", url: "https://music.youtube.com" },
+  { id: "5", name: "Next.js", url: "https://nextjs.org/" },
+  { id: "6", name: "TaliwindCSS", url: "https://tailwindcss.com/" },
+  { id: "7", name: "環境資料開放平臺", url: "https://data.moenv.gov.tw/" },
+  { id: "8", name: "氣象開放資料平台", url: "https://opendata.cwa.gov.tw/" },
 ]
 
 // 從 URL 獲取域名
